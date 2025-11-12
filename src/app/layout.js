307af0ff435189206497a7011ac821074e9a5d33
ajinas001 <ajinasp001@gmail.com>
2025-11-12 17:11:14 +0530

    Fix Lenis + react-icons imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import useLenis from "@/lib/lenis";
 import LenisWrapper from "@/components/LenisWrapper";
 
 const geistSans = Geist({

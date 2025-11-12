89bddc4edc99ea4b154b3cbfda7c6ea35c83a9a8
ajinas001 <ajinasp001@gmail.com>
2025-11-12 17:24:41 +0530

    updated navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,19 @@
-'use client'
-
-import React, { useState, useEffect } from 'react';
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/HeroSection';
-import Footer from '@/components/Footer';
-import Aboutus from '@/components/Aboutus';
-
-
-
+"use client";
+import React, { useState } from "react";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import Footer from "@/components/Footer";
+import Aboutus from "@/components/Aboutus";
 
 const BookerAccountingWebsite = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
-
-
-
-  // Scroll handler for Navbar
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-
-
 
   return (
     <div className="min-h-screen bg-primary text-foreground">
-
-
-
-      <Navbar scrolled={scrolled} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <Navbar />
       <HeroSection />
-      <Aboutus/>
-      <Footer/>
-
-
+      <Aboutus />
+      <Footer />
     </div>
   );
 };
